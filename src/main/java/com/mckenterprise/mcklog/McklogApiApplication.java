8452ba561c27124352b88562a45bb74f8a43075b
Melquisedeque Marins <melck_junior@hotmail.com>
2022-01-14 17:02:48 -0300

    Criação da classe cliente

---
+++ src/main/java/com/mckenterprise/mcklog/McklogApiApplication.java
@@ -0,0 +1,13 @@
+package com.mckenterprise.mcklog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McklogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McklogApiApplication.class, args);
+	}
+
+}
