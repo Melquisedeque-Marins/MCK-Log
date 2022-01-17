669302cc5c468120671e439bddb7986741cd2a73
Melquisedeque Marins <melck_junior@hotmail.com>
2022-01-16 22:48:22 -0300

    Cliente end-points e validate

---
+++ src/main/java/com/mckenterprise/mcklog/repositories/ClienteRepository.java
@@ -0,0 +1,16 @@
+package com.mckenterprise.mcklog.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mckenterprise.mcklog.entities.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+	List<Cliente> findByNome(String nome);
+	List<Cliente> findByNomeContaining(String nome);
+	
+}
